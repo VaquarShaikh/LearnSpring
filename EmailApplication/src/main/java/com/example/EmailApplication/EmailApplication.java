83d402022b35b61c4c3581a323cb5fe73537e313
VaquarShaikh <vaquarshaikh253@gmail.com>
2022-04-05 22:46:41 +0530

    Configuring using config files

--- EmailApplication/src/main/java/com/example/EmailApplication/EmailApplication.java
+++ EmailApplication/src/main/java/com/example/EmailApplication/EmailApplication.java
@@ -13,7 +13,7 @@ public class EmailApplication {
 	public static void main(String[] args) {
 
 		ApplicationContext applicationContext =
-				new ClassPathXmlApplicationContext("beans.xml");
+				new AnnotationConfigApplicationContext(AppConfig.class);
 		EmailClient emailClient = applicationContext.getBean("emailClient", EmailClient.class);
 		emailClient.sendEmail("Hey, " +
 				"This is my first email message.");
